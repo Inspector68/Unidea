75713196687ce9ea0eda9d308b2cc75b06d1ae83
Inspector68 <32607215+Inspector68@users.noreply.github.com>
2026-01-29 22:28:46 +0300

    Delete index.tsx

--- index.tsx
+++
@@ -1,5 +0,0 @@
-const res = await fetch("https://unidea-backend.onrender.com/process", {
-  method: "POST",
-  headers: { "Content-Type": "application/json" },
-  body: JSON.stringify({ text: question })
-});
